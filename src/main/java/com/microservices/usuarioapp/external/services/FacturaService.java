cdbfbfdcab8a75cb6183f8c056a8f546d55fcaeb
Brando Elí Carrillo Pérez <belicarrillo30@gmail.com>
2024-06-29 20:59:54 -0500

    external services URL's changed

--- src/main/java/com/microservices/usuarioapp/external/services/FacturaService.java
+++ src/main/java/com/microservices/usuarioapp/external/services/FacturaService.java
@@ -13,12 +13,12 @@ public class FacturaService {
     private RestTemplate restTemplate;
 
     public Factura generate(Factura factura) {
-        final String URL = "http://gavasoftcomprasapp/v1/facturas/generar-nueva";
+        final String URL = "http://COMPRAS-APP/v1/facturas/generar-nueva";
         return restTemplate.postForObject(URL, factura, Factura.class);
     }
 
     public List<Factura> listPagadasByClienteNumDocumento(String clienteNumDocumento) {
-        final String URL = "http://gavasoftcomprasapp/v1/facturas/clientes/{clienteNumDocumento}/pagadas";
+        final String URL = "http://COMPRAS-APP/v1/facturas/clientes/{clienteNumDocumento}/pagadas";
         return restTemplate.getForObject(URL, List.class, clienteNumDocumento);
     }
 }
